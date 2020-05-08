3a05a9a0688da46a7e4655c99aed199b776d7f27
Kovacs Akos <akoskovacs1980@gmail.com>
2020-05-08 23:35:56 +0200

    Make GT4500.java testable by ctor injection. Mock objects used in unit tests. Fixed a bug in GT4500.java when all torpedo stores are firing: the second torpedo store's fire method wasn't called if the first torpedo store was successfull because of the logic operator short circuiting.

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,9 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500(TorpedoStore primaryTorpedoStore, TorpedoStore secondaryTorpedoStore) {
+    this.primaryTorpedoStore = primaryTorpedoStore;
+    this.secondaryTorpedoStore = secondaryTorpedoStore;
   }
 
   public boolean fireLaser(FiringMode firingMode) {
@@ -82,7 +82,7 @@ public class GT4500 implements SpaceShip {
       		firingSuccess = primaryTorpedoStore.fire(1);
       	}
       	if (!secondaryTorpedoStore.isEmpty()) {
-      		firingSuccess = firingSuccess || secondaryTorpedoStore.fire(1);
+      		firingSuccess = secondaryTorpedoStore.fire(1) || firingSuccess;
       	}
         break;
     }
